087f68b20c77d7bb6d6116d48687b15488d4703a
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-05-31 21:40:54 -0300

    add UserPage component with welcome message

---
+++ app/userpage.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function UserPage() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 20 }}>Bem-vindo à User Page!</Text>
+    </View>
+  );
+}
