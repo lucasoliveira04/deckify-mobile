a1e79629fb6d10fe3054c181205099aa763bb76f
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-06-01 15:28:25 -0300

    feat: enhance useDeck hook with context validation and improved deck creation logic

--- src/hooks/useDeck.ts
+++ src/hooks/useDeck.ts
@@ -1,23 +1,35 @@
 import { useState } from "react";
+import { DeckContextValidation } from "../validations/deckValidation/deckContextValidation";
 import { DeckNameValidation } from "../validations/deckValidation/deckNameValidation";
 
 export const useDeck = () => {
   const [deck, setDeck] = useState({
     nameDeck: "",
+    contexto: "",
   });
 
   const createDeck = () => {
-    const deckValidator = new DeckNameValidation();
+    const deckNameValidator = new DeckNameValidation();
+    const deckContatextValidator = new DeckContextValidation();
 
-    const validationResult = deckValidator.validate(
+    const validationNameResult = deckNameValidator.validate(
       "Nome do Deck",
       deck.nameDeck
     );
 
-    if (validationResult !== true) {
+    const validationContextResult = deckContatextValidator.validate(
+      "Contexto do Deck",
+      deck.contexto
+    );
+
+    if (validationNameResult !== true || validationContextResult !== true) {
       return false;
     }
 
+    setDeck({
+      nameDeck: "",
+      contexto: "",
+    });
     return true;
   };
 
